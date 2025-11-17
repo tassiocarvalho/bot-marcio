345b1c4653c8ed9a2bd77c10902164c4ba40cb0a
tassiocarvalho <90158519+tassiocarvalho@users.noreply.github.com>
2025-11-17 11:38:23 -0300

    att

--- src/config.js
+++ src/config.js
@@ -8,10 +8,10 @@ const __dirname = path.dirname(__filename);
 export const PREFIX = "/";
 
 // Emoji do bot (mude se preferir).
-export const BOT_EMOJI = "🤖";
+export const BOT_EMOJI = "😎";
 
 // Nome do bot (mude se preferir).
-export const BOT_NAME = "Márcio Bot";
+export const BOT_NAME = "Tassio BOT";
 
 // LID do bot.
 // Para obter o LID do bot, use o comando <prefixo>lid respondendo em cima de uma mensagem do número do bot
