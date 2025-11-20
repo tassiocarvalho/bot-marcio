3484465fde3add9940f843f97197789ea9bec262
Tassio Carvalho <90158519+tassiocarvalho@users.noreply.github.com>
2025-11-20 08:34:58 -0300

    add

--- src/commands/admin/hide-tag.js
+++ src/commands/admin/hide-tag.js
@@ -3,15 +3,37 @@ import { PREFIX } from "../../config.js";
 export default {
   name: "hide-tag",
   description: "Este comando marcará todos do grupo",
-  commands: ["hide-tag", "to-tag"],
-  usage: `${PREFIX}hidetag motivo`,
+  commands: ["hide-tag", "to-tag", "hidtag"],
+  usage: `${PREFIX}hidtag motivo`,
+
   /**
    * @param {CommandHandleProps} props
    */
-  handle: async ({ fullArgs, sendText, socket, remoteJid, sendReact }) => {
+  handle: async ({ fullArgs, sendText, socket, remoteJid, sendReact, quoted, type }) => {
     const { participants } = await socket.groupMetadata(remoteJid);
     const mentions = participants.map(({ id }) => id);
     await sendReact("📢");
-    await sendText(`📢 Marcando todos!\n\n${fullArgs}`, mentions);
+
+    let msgParaEnviar = "";
+
+    // ----- 1. Se respondeu uma mensagem -----
+    if (quoted) {
+      // Se a mensagem respondida for texto
+      if (quoted.msg && quoted.msg.conversation) {
+        msgParaEnviar = quoted.msg.conversation;
+      } else {
+        return await sendText("❗ Marque uma mensagem **de texto**!", mentions);
+      }
+    }
+    // ----- 2. Se escreveu algo após o comando -----
+    else if (fullArgs.trim().length > 0) {
+      msgParaEnviar = fullArgs.trim();
+    }
+    // ----- 3. Se usou o /hidtag sozinho -----
+    else {
+      msgParaEnviar = "📢 Marcando todos!";
+    }
+
+    await sendText(msgParaEnviar, mentions);
   },
 };
